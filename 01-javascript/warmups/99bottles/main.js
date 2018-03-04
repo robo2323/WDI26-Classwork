8415f56cca06ac6f4d41e34e647aad1460c478c6
robo2323 <rob.adam.h@gmail.com>
2018-03-05 10:18:40 +1100

    one console.log

--- 01-javascript/warmups/99bottles/main.js
+++ 01-javascript/warmups/99bottles/main.js
@@ -1,21 +1,17 @@
 const countBeers = (numBottles = 99, endCount = 0) => {
   for (let i = numBottles; i >= endCount; i--) {
-    if (i > 1) {
-      console.log(
-        `${i} botles of beer on the wall.....Take one down, pass it around, ${i -
-          1} bottles of beer on the wall...`
-      );
-    }
-    if (i === 1) {
-      console.log(
-        '1 bottle of beer on the wall, 1 bottle of beer. Take it down and pass it around, no more bottles of beer on the wall.'
-      );
-    }
-    if (i === 0) {
-      console.log(`No more bottles of beer on the wall, no more bottles of beer.
-      Go to the store and buy some more, ${numBottles} bottles of beer on the wall.`);
-    }
+    console.log(
+      `${
+        i > 0
+          ? `${i} botle${
+            i > 1 ? 's' : ''
+          } of beer on the wall.....Take one down, pass it around, ${
+            i - 1 === 0 ? 'no more' : i - 1
+          } bottles of beer on the wall...`
+          : `No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, ${numBottles} bottles of beer on the wall.`
+      }`
+    );
   }
 };
 
-countBeers(20,10);
+countBeers();
